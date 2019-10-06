96d749fa0984c73e06484aef8a087b207dc5ba1c
Peter McNaught <petermmcnaught@gmail.com>
2019-10-06 19:10:52 +1100

    Added input and hook to reverser endpoint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { apiResponse: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
   }
 
   callAPI() {
@@ -15,18 +21,40 @@ class App extends Component {
       .catch(err => err);
   }
 
+  sendAPI() {
+    return fetch(`http://localhost:9000/reverser/?${this.state.value}`)
+      .then(res => res.text())
+      .then(res => this.setState({ peter: res }))
+      .catch(err => err);
+  }
+
+  async handleSubmit(event) {
+    await this.sendAPI();
+    alert('dettimbus saw eman: ' + this.state.peter);
+    event.preventDefault();
+  }
+
   componentDidMount() {
     this.callAPI();
   }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-intro">{this.state.apiResponse}</p>
         </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
